527623ce3ed5dc204fa7fe211e7a3db0b4c87df2
papon <85781719+oya254@users.noreply.github.com>
2023-12-21 21:43:50 +0700

    :zap:

--- phpforStudent/home.php
+++ phpforStudent/home.php
@@ -2,57 +2,53 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@200;500&family=Sarabun&display=swap" rel="stylesheet">
-    <title>My Home Page</title>
-    <!-- #### ส่วนของ CSS ตกแต่งเว็บไซต์ ### -->
-    <link href="https://fonts.googleapis.com/css?family=Abel&display=swap" rel="stylesheet">
-    <style>
-    body, html {
-    background-size: cover;
-    font-family: 'Kanit', sans-serif;
-    }
-    a{
-        text-decoration: none;
-        color: black;
-    }
-    img,H2{
-
-        display: inline;
-    }
-    .row {
-    display: flex;
-    }
-
-    .column {
-    flex: 50%;
-    }
-    </style> 
-
+    <title>MyPHP</title>
 </head>
-<!-- ตั้งรูปพื้นหลังที่นี่ -->
-<body background="./.....png">
-    <div class="row">
-        
-        <div class="column" align="right" style="padding-right: 20px;padding-top: 40px;">
-            <!-- ใส่ลิงก์ในหน้าเว็บที่นี่ -->
-            <a href="home.php"><strong>HOME</strong></a><strong> |
-            <a href="php1.php">PHP 1</a> |
-            <a href="php2.php">PHP 2</a> |
-            <a href="loop.php">LOOP</a> |
-			<a href="array.php">ARRAY</a> 
-            </strong>
-        </div>
-		
-    </div>
-	<hr/>
-    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<body>
 
-<!-- ใส่ CODE PHP ที่นี่ -->				  
+<?php
+// กำหนดตัวแปร $fullname
+$fullname = "harry potter";
 
+// แยกชื่อและนามสกุล
+list($firstname, $lastname) = explode(" ", $fullname);
+
+// กำหนดชื่อเล่น
+$nickname = "HARRY";
+
+// คำนวณอายุ
+$birthdate = strtotime("2005-01-06"); // วันเกิดตัวอย่าง
+$currentDate = time();
+$age = date("Y", $currentDate) - date("Y", $birthdate);
+
+// วันที่บันทึกข้อมูล
+$update = date("Y-m-d");
+?>
+
+<table border="1">
+    <tr>
+        <td>Firstname</td>
+        <td><?php echo $firstname; ?></td>
+    </tr>
+    <tr>
+        <td>Lastname</td>
+        <td><?php echo $lastname; ?></td>
+    </tr>
+    <tr>
+        <td>Nickname</td>
+        <td><?php echo $nickname; ?></td>
+    </tr>
+    <tr>
+        <td>Age</td>
+        <td><?php echo $age; ?></td>
+    </tr>
+    <tr>
+        <td>Update</td>
+        <td><?php echo $update; ?></td>
+    </tr>
+</table>
 
 </body>
 </html>
